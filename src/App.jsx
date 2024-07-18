1631be80aa47b944f6e7ca50216fa4dc661dad98
flo0f <144050720+flo0f@users.noreply.github.com>
2024-07-18 11:49:34 +0500

    project init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="bg-teal-800 text-white">There{"'"}s will be timer</div>
+    </div>
+  );
+}
+
+export default App;

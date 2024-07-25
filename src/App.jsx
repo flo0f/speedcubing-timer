96a23bd3a9129297a7418268c120fa7f47c866e3
flo0f <144050720+flo0f@users.noreply.github.com>
2024-07-25 17:14:03 +0500

    added scramble generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Timer from "./components/Timer";
+import Scramble from "./components/Scramble";
 function App() {
   return (
     <>
-      <div className="h-full bg-teal-800 text-white flex justify-center items-center">
+      <div className="h-full bg-teal-800 text-white flex flex-col items-center">
+        <Scramble />
         <Timer />
       </div>
     </>
